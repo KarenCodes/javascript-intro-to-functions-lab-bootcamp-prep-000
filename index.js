0a783700206e57c9db3f6da9b0c7b287a9ade3a7
Karen Neumann <karen@websavvystudio.com>
2018-01-16 15:04:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,16 @@ function logWhisper (string) {
   string = string.toLowerCase()
   console.log(string)
 }
+function sayHiToGrandma (string) {
+  var uppercase = string.toUpperCase()
+  var lowercase = string.toLowerCase()
+  if (string === "I love you, Grandma.") {
+    return("I love you, too.")
+  } 
+  else if (string === uppercase) {
+    return("YES INDEED!")
+  }
+  else if (string === lowercase) {
+     return("I can't hear you!")
+  }
+}
